651b71d84bb6e77b59ee29e45e0da8c9ba855492
BarisSaracc <96023912+BarisSaracc@users.noreply.github.com>
2025-10-24 13:47:39 +0300

    Add files via upload

---
+++ Custom_AXI_UART_IP/Ultra_UART_v1_2.vhd
@@ -0,0 +1,187 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Ultra_UART_v1_2 is
+	generic (
+		-- Users to add parameters here
+
+		-- User parameters ends
+		-- Do not modify the parameters beyond this line
+
+
+		-- Parameters of Axi Slave Bus Interface S00_AXI
+		C_S00_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S00_AXI_ADDR_WIDTH	: integer	:= 4
+	);
+	port (
+		-- Users to add ports here
+        TX_out : out std_logic;
+
+		-- User ports ends
+		-- Do not modify the ports beyond this line
+
+
+		-- Ports of Axi Slave Bus Interface S00_AXI
+		s00_axi_aclk	: in std_logic;
+		s00_axi_aresetn	: in std_logic;
+		s00_axi_awaddr	: in std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+		s00_axi_awprot	: in std_logic_vector(2 downto 0);
+		s00_axi_awvalid	: in std_logic;
+		s00_axi_awready	: out std_logic;
+		s00_axi_wdata	: in std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+		s00_axi_wstrb	: in std_logic_vector((C_S00_AXI_DATA_WIDTH/8)-1 downto 0);
+		s00_axi_wvalid	: in std_logic;
+		s00_axi_wready	: out std_logic;
+		s00_axi_bresp	: out std_logic_vector(1 downto 0);
+		s00_axi_bvalid	: out std_logic;
+		s00_axi_bready	: in std_logic;
+		s00_axi_araddr	: in std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+		s00_axi_arprot	: in std_logic_vector(2 downto 0);
+		s00_axi_arvalid	: in std_logic;
+		s00_axi_arready	: out std_logic;
+		s00_axi_rdata	: out std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+		s00_axi_rresp	: out std_logic_vector(1 downto 0);
+		s00_axi_rvalid	: out std_logic;
+		s00_axi_rready	: in std_logic
+	);
+end Ultra_UART_v1_2;
+
+architecture arch_imp of Ultra_UART_v1_2 is
+
+COMPONENT ila_uart
+
+PORT (
+	clk : IN STD_LOGIC;
+
+
+
+	probe0 : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+	probe1 : IN STD_LOGIC_VECTOR(7 DOWNTO 0)
+);
+END COMPONENT  ;
+	-- component declaration
+	component Ultra_UART_v1_2_S00_AXI is
+		generic (
+		C_S_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S_AXI_ADDR_WIDTH	: integer	:= 4
+		);
+		port (
+		S_AXI_ACLK	: in std_logic;
+		S_AXI_ARESETN	: in std_logic;
+		S_AXI_AWADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_AWPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_AWVALID	: in std_logic;
+		S_AXI_AWREADY	: out std_logic;
+		S_AXI_WDATA	: in std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_WSTRB	: in std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+		S_AXI_WVALID	: in std_logic;
+		S_AXI_WREADY	: out std_logic;
+		S_AXI_BRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_BVALID	: out std_logic;
+		S_AXI_BREADY	: in std_logic;
+		S_AXI_ARADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_ARPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_ARVALID	: in std_logic;
+		S_AXI_ARREADY	: out std_logic;
+		S_AXI_RDATA	: out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_RRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_RVALID	: out std_logic;
+		S_AXI_RREADY	: in std_logic;
+				
+				-- User ports ekleniyor
+		slv_reg0_out : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		slv_reg1_out : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0)
+		);
+
+		
+	end component Ultra_UART_v1_2_S00_AXI;
+	
+	signal reg0 : std_logic_vector(31 downto 0);
+signal reg1 : std_logic_vector(31 downto 0);
+signal tx_line : std_logic;
+signal nd_prev : std_logic := '0';
+signal nd_pulse : std_logic;
+
+signal rst_int : std_logic;
+
+component UART_Generator is
+		port (
+		CLK : in std_logic;
+        TX : out std_logic;
+        RST : in std_logic;
+        binary: in std_logic_vector(7 downto 0);
+        ND: in std_logic
+		);
+	end component UART_Generator;
+
+begin
+
+rst_int <= not s00_axi_aresetn;
+
+UART_ILA0 : ila_uart
+PORT MAP (
+	clk => s00_axi_aclk,
+	probe0 => reg1(0 downto 0),
+	probe1 => reg0(7 downto 0)
+);
+
+-- Instantiation of Axi Bus Interface S00_AXI
+Ultra_UART_v1_2_S00_AXI_inst : Ultra_UART_v1_2_S00_AXI
+	generic map (
+		C_S_AXI_DATA_WIDTH	=> C_S00_AXI_DATA_WIDTH,
+		C_S_AXI_ADDR_WIDTH	=> C_S00_AXI_ADDR_WIDTH
+	)
+	port map (
+		S_AXI_ACLK	=> s00_axi_aclk,
+		S_AXI_ARESETN	=> s00_axi_aresetn,
+		S_AXI_AWADDR	=> s00_axi_awaddr,
+		S_AXI_AWPROT	=> s00_axi_awprot,
+		S_AXI_AWVALID	=> s00_axi_awvalid,
+		S_AXI_AWREADY	=> s00_axi_awready,
+		S_AXI_WDATA	=> s00_axi_wdata,
+		S_AXI_WSTRB	=> s00_axi_wstrb,
+		S_AXI_WVALID	=> s00_axi_wvalid,
+		S_AXI_WREADY	=> s00_axi_wready,
+		S_AXI_BRESP	=> s00_axi_bresp,
+		S_AXI_BVALID	=> s00_axi_bvalid,
+		S_AXI_BREADY	=> s00_axi_bready,
+		S_AXI_ARADDR	=> s00_axi_araddr,
+		S_AXI_ARPROT	=> s00_axi_arprot,
+		S_AXI_ARVALID	=> s00_axi_arvalid,
+		S_AXI_ARREADY	=> s00_axi_arready,
+		S_AXI_RDATA	=> s00_axi_rdata,
+		S_AXI_RRESP	=> s00_axi_rresp,
+		S_AXI_RVALID	=> s00_axi_rvalid,
+		S_AXI_RREADY	=> s00_axi_rready,
+		
+		 slv_reg0_out => reg0,
+        slv_reg1_out => reg1
+        
+
+	);
+
+
+	-- Add user logic here
+UART_Generator_inst : UART_Generator
+    port map (
+        CLK    => s00_axi_aclk,
+        RST    => rst_int,
+        TX     => tx_line,
+        binary => reg0(7 downto 0),
+        ND     => reg1(0)
+    );
+-- Clock process
+process(s00_axi_aclk)
+begin
+    if rising_edge(s00_axi_aclk) then
+        nd_prev <= reg1(0);
+        nd_pulse <= reg1(0) and not nd_prev;
+
+    end if;
+end process;
+
+TX_out <= tx_line;
+	-- User logic ends
+
+end arch_imp;
